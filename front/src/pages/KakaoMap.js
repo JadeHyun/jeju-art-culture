f63b6dd56dd3d3fce9f2f8ad3b098d60aef0e761
pitang <wpeffect@gmail.com>
2022-12-02 11:30:54 +0900

    ✨ 카카오 지도 위에 여러개 마커 생성

--- front/src/pages/KakaoMap.js
+++ front/src/pages/KakaoMap.js
@@ -60,6 +60,40 @@ const KakaoMap = () => {
         latlng: new kakao.maps.LatLng(33.507071, 126.492764),
       },
     ];
+
+    var imageSrc =
+      "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+
+    // 여러개 마커 찍기
+    for (let i = 0; i < positions.length; i++) {
+      // 마커 이미지의 이미지 크기 입니다
+      var imageSize = new kakao.maps.Size(24, 35);
+
+      // 마커 이미지를 생성합니다
+      var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+      // 마커를 생성할 때 필요한 position 을 미리 설정해놓습니다
+      let markerId = positions[i].id;
+
+      var marker = new kakao.maps.Marker({
+        map: map, // 마커를 표시할 지도
+        position: positions[i].latlng, // 마커를 표시할 위치
+        title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+        image: markerImage, // 마커 이미지
+        id: markerId,
+      });
+
+      // 마커에 표시할 인포윈도우를 생성합니다
+      var infowindow = new kakao.maps.InfoWindow({
+        content: positions[i].content, // 인포윈도우에 표시할 내용
+      });
+      // 이벤트 리스너로는 클로저를 만들어 등록합니다
+      // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
+
+      kakao.maps.event.addListener(marker, "click", function click() {
+        console.log("click");
+      });
+    }
   }, []);
 
   return (
@@ -73,7 +107,7 @@ export default KakaoMap;
 
 let MapContainer = styled.div`
   border-radius: 20px;
-  width: 310px;
-  height: 380px;
+  width: 500px;
+  height: 400px;
   margin: 0 20px;
 `;
