9f930b81ccdcdab54c9be031edbb7a2f89c96a69
pitang <wpeffect@gmail.com>
2022-12-02 16:04:43 +0900

    💄 이미지변경

--- front/src/components/Modal.js
+++ front/src/components/Modal.js
@@ -1,46 +1,51 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import musaImg from "../assets/musa.png";
 
-export default function Modal({ a, setA }) {
+export default function Modal({ isModal, setIsModal }) {
   return (
     <Container>
-      <div className="musaImg">
-        <img src={musaImg} alt="musa 책방 이미지" />
-        <button onClick={() => setA(!a)}>X</button>
+      <div>
+        책방무사
+        <button onClick={(isModal) => setIsModal(!isModal)}>X</button>
       </div>
       <div>
-        <p>주소 : 제주 서귀포시 성산읍 수시로10번길 3</p>
         <p>
-          인스타그램 : <a href="https://storemusa.com/">storemusa</a>
+          제주 동쪽 성산 일출봉 근처 수산리라고 하는 작은 마을에 있는
+          책방입니다.
         </p>
-        <p>연락처 : 010-6584-6571</p>
+        <p>제주섬과 모두의 무사한 하루를 진심으로 바라며 책방 문을 엽니다.</p>
         <pre>
-          제주 동쪽 성산 일출봉 근처 수산리라고 하는 <br />
-          작은 마을에 있습니다. 간판이 없어 찾기가 쉽지
+          인스타그램 : <a href="https://storemusa.com/">storemusa</a>
+        </pre>
+        <pre>
+          2023년 2월 25일 토요일 저녁 19시부터 진행될 공연을 준비중입니다.
           <br />
-          않을 수 있습니다. 그럴 때는 <br />
-          ‘수산 초등학교’를 찾아 앞에서 두리번 거리다
+          뮤지션 백예린이 준비한 추천 코스를 다녀오신 분들께는
           <br />
-          어쩐지 측은하게 생긴 점방 건물이
-          <br />
-          보인다면 그곳이 책방입니다.
-          <br />
-          제주섬과 모두의 무사한 하루를
-          <br />
-          진심으로 바라며 책방 문을 엽니다.
+          공연 티켓 1매와 함께 뮤지션과 관련된 NFT 특전 선물이 증정됩니다.
+          <p>(선착순 수량 : 20매)</p>
         </pre>
       </div>
       <div className="wrapper">
-        <Link to="qrcheck">QR체크하기</Link>
+        방문 확인을 위해 해당 지점에서&nbsp;<Link to="qrcheck">QR</Link>을 꼭
+        찍어주세요.
       </div>
+      <p>뮤지션 백예린의 코스</p>
+      <pre>
+        1. 광치기 해변 <br />
+        2. 성산 일출봉 정상 <br />
+        3. 고궁 보말칼국수 <br />
+        4. 카페 나무아래 <br />
+        5. 스피닝울프 펍
+      </pre>
     </Container>
   );
 }
 
 const Container = styled.div`
   border: 1px solid black;
+  border-radius: 20px;
   box-sizing: border-box;
   display: flex;
   flex-direction: column;
@@ -82,5 +87,11 @@ const Container = styled.div`
     display: flex;
     align-items: end;
     margin-bottom: 20px;
+  }
+
+  button {
+    width: 30px;
+    height: 25px;
+    float: right;
   }
 `;
